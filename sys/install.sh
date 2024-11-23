9dd5369d4b9b6315386f998304010ac48cd3f84b
e <e@es-MacBook-Pro.home>
2024-11-23 02:41:47 +0100

    installer fixes

--- sys/install.sh
+++ sys/install.sh
@@ -20,14 +20,15 @@ fi
 sudo cp ./sys/$SCRIPT /usr/local/bin
 # replace user_name with your user name in script file
 sudo sed -i -e "s/user_name/$USER/g" /usr/local/bin/$SCRIPT
-# make the script executable
+# make the script executable for the user
 sudo chmod +x /usr/local/bin/$SCRIPT
+sudo chown $USER /usr/local/bin/$SCRIPT
 # Make a user directory if it does not exist
 mkdir -p ~/.config/systemd/user
 # Enable user's systemd instance to run after logout (as root):
 sudo loginctl enable-linger $USER
 # copy service file
-sudo cp ./sys/$SERVICE /etc/systemd/system/.config/systemd/user
+sudo cp ./sys/$SERVICE /home/$USER/.config/systemd/user
 # replace user_name with your user name in service file
 sudo sed -i -e "s/user_name/$USER/g" /home/$USER/.config/systemd/user/$SERVICE
 # reload systemd
